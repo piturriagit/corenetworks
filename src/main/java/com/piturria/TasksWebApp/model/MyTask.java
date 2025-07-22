6ae9877052e2ee932c290ae56e21ed43c9910809
piturria <patricia.iturriaganez@gmail.com>
2025-07-22 12:30:12 +0200

    Configure h2 database and initial data load

--- src/main/java/com/piturria/TasksWebApp/model/MyTask.java
+++ src/main/java/com/piturria/TasksWebApp/model/MyTask.java
@@ -1,19 +1,23 @@
 package com.piturria.TasksWebApp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
+import org.antlr.v4.runtime.misc.NotNull;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @Entity
+@Table(name="tasks")
 public class MyTask {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
     private String title;
     private String description;
     private LocalDateTime creationDate;
