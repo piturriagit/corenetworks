4fa45c3be137fa0088ced26aa97dc4c7a25257b1
piturria <patricia.iturriaganez@gmail.com>
2025-07-22 14:29:03 +0200

    replace field injection by constructor one

--- src/main/java/com/piturria/TasksWebApp/service/MyTaskService.java
+++ src/main/java/com/piturria/TasksWebApp/service/MyTaskService.java
@@ -5,21 +5,17 @@ import com.piturria.TasksWebApp.repository.MyTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class MyTaskService {
 
-    @Autowired
     private MyTaskRepository repository;
 
-//    //Constructor injection
-//    public MyTaskService(MyTaskRepository repository) {
-//        this.repository = repository;
-//    }
+    //Constructor injection
+    public MyTaskService(MyTaskRepository repository) {
+        this.repository = repository;
+    }
 
     public List<MyTask> getAllTasks() {
         return repository.findAll();
