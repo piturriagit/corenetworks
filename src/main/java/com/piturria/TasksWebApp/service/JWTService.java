c5d312ecef8223cba800e4816b9e92f96a1cd7ac
piturria <patricia.iturriaganez@gmail.com>
2025-07-28 00:52:20 +0200

    generate JWT with keyGenerator(HmacSHA256) but verification is not ok

--- src/main/java/com/piturria/TasksWebApp/service/JWTService.java
+++ src/main/java/com/piturria/TasksWebApp/service/JWTService.java
@@ -5,7 +5,11 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,7 +20,18 @@ public class JWTService {
     // 60 length so it produces >256bits so JWT can produce token
     // The JWT JWA Specification (RFC 7518, Section 3.2) states that keys used
     // with HMAC-SHA algorithms MUST have a size >= 256 bits
-    private String fakeKey="3wcP8AeBh5xAzPBowdHJW3cprsV0YmPfhsMWHycKHtCBt2LqiDbYGg0a8Cen";
+    //private String fakeKey="3wcP8AeBh5xAzPBowdHJW3cprsV0YmPfhsMWHycKHtCBt2LqiDbYGg0a8Cen";
+    private String stringKey ="";
+
+    public JWTService() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            stringKey=Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String generateToken(String username) {     //https://www.jwt.io
 //        https://www.scottbrady.io/tools/jwt
@@ -30,7 +45,7 @@ public class JWTService {
                 .claims().add(claims)
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + (1000*60*10)))      //expires in 10 minutes
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*10))      //expires in 10 minutes
                 .and()
                 .signWith(getKey())
                 .compact();
@@ -38,7 +53,8 @@ public class JWTService {
     }
 
     private Key getKey() {
-        byte [] keyBytes = Decoders.BASE64.decode(fakeKey);
+        byte [] keyBytes = Decoders.BASE64.decode(stringKey);
         return Keys.hmacShaKeyFor(keyBytes);
+        //validate on jwt.io with base64 flag: eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYXZpbiIsImlhdCI6MTcyMjE3MjMwNiwiZXhwIjoxNzIyM
     }
 }
