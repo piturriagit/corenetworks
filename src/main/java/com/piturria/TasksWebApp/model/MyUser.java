b5b6e4fd27a3350e9b2ff885a2678b79af95e180
piturria <patricia.iturriaganez@gmail.com>
2025-07-29 02:54:10 +0200

    jpa: user unique, user and password not null

--- src/main/java/com/piturria/TasksWebApp/model/MyUser.java
+++ src/main/java/com/piturria/TasksWebApp/model/MyUser.java
@@ -2,30 +2,15 @@ package com.piturria.TasksWebApp.model;
 
 import jakarta.persistence.*;
 
-/*
-CREATE TABLE `securitydb`.`users` (
-  `id` INT NOT NULL,
-  `username` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
-INSERT INTO users (id, username, password)  VALUES
-  (1,"ana","$2a$12$Mb5D8LBPRG.vU45sujldm.NHHCmLZEUaADEkMi5Ej.38JpynXQnei")
-, (2,"bea","$2a$12$0blOmOf3pGxiHHehZXRj3OddDW2pH.4PP0eTA5s2NU4mKc0qrNqC.")
-, (3, "carlos", "$2a$12$qME8Ux5SKINLwS5Lfit4KeTMCvzhZzqUsG.JPwHc.CLcUQT49qHum")
-;
--- BCrypt(12,a@123) = $2a$12$Mb5D8LBPRG.vU45sujldm.NHHCmLZEUaADEkMi5Ej.38JpynXQnei
--- https://bcrypt-generator.com
-SELECT * FROM users;
- */
-
 @Entity
 @Table(name="users")
 public class MyUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false, unique=true)
     private String username;
+    @Column(nullable = false)
     private String password;
 
     public int getId() {
